14f17fc91d62fc2d44a120f7bc38de0ffe69e981
= <=>
2026-03-13 14:30:06 -0500

    pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://dkalvex.github.io',
+  base: '/somosiglesia-web-app'
+});
